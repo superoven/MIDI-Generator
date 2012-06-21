344e0c39ab988e7e284c29cf2ef53a07202907d8
Taylor Ritenour <taylorritenour@berkeley.edu>
2012-06-21 15:33:34 -0700

    minor addition to chromosome class

--- src/chromosome.cpp
+++ src/chromosome.cpp
@@ -13,7 +13,7 @@
 #include "chromosome.h"
 using namespace std;
 
-int chromosome::numchromosomes = 0;
+int chromosome::numchromosomes = 0; //Static data needs to be declared like this or it throws a shit fit.
 
 chromosome::chromosome(int len)
 {
@@ -24,7 +24,7 @@ chromosome::chromosome(int len)
   length = len;
   fitness = -1;
   numchromosomes++;
-  for(int i = 0; i < len; i++) bytes[i] = (char)(rand() % GENE_POSSIBILITIES);
+  for(int i = 0; i < len; i++) bytes[i] = (char)(rand() % GENE_POSSIBILITIES); //Generate random bytes for the chromosome
 }
 
 chromosome::~chromosome()
@@ -45,22 +45,22 @@ void chromosome::printChromosome()
     {
       int value1 = bytes[i] & 0xF;
       int value2 = (bytes[i] & 0xF0) >> 4;
-      cout << hex << uppercase << value2 << value1 << (((i % PRINT_BYTES_PER_LINE) != PRINT_BYTES_PER_LINE - 1) ? " " : "\n");
+      cout << hex << uppercase << value2 << value1 << (((i % PRINT_BYTES_PER_LINE) != PRINT_BYTES_PER_LINE - 1) ? " " : "\n"); //It just works. Trust me.
     }
   if (i % PRINT_BYTES_PER_LINE) cout << endl;
   cout << "Length: " << dec << length << endl; 
   cout << "Fitness: " << dec << fitness << endl;
 }
 
-char chromosome::getByte(int num) { return bytes[num]; }
+char chromosome::getByte(int num) { return bytes[num]; } //Get the 'num' byte
 
-long chromosome::getLength() { return length; }
+long chromosome::getLength() { return length; } //Get the length of the chromosome
 
-double chromosome::getFitness() { return fitness; }
+double chromosome::getFitness() { return fitness; } //Get the fitness of the chromosome
 
-void chromosome::setByte(int num, char A) { bytes[num] = A; }
+void chromosome::setByte(int num, char A) { bytes[num] = A; } //Set the byte of the chromosome. Please don't use this ever.
   
-void chromosome::setFitness(double arg) { fitness = ((arg > 0) ? arg : 0); }
+void chromosome::setFitness(double arg) { fitness = ((arg > 0) ? arg : 0); } //Set the fitness of the chromosome, defaults to 0
 
 void chromosome::mutate()
 {
@@ -80,15 +80,18 @@ void crossover(chromosome& a, chromosome& b) 	//combine chromosome a and b
   char* buffer1 = new char [bufferlen];
   char* buffer2 = new char [bufferlen];
 
-  for (int i = 0; i < bufferlen; i++) {
+  for (int i = 0; i < bufferlen; i++) { //Store the buffers
     buffer1[i] = a.getByte(point + i);
     buffer2[i] = b.getByte(point + i);
   }
-  for (int i = 0; i < bufferlen; i++) {
+  for (int i = 0; i < bufferlen; i++) { //Place the data where it should be reallocated
     a.setByte(point + i, buffer2[i]);
     b.setByte(point + i, buffer1[i]);
   }
-    
-  delete buffer1;
+  
+  a.setFitness(-1); //These are totally new, so if they had a fitness, they are back to not having one.
+  b.setFitness(-1);
+
+  delete buffer1; //Clean up allocated memory
   delete buffer2;
 }
