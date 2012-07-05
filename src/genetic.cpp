41a3cb98484df55a8112dd159a5933ea03ef16df
Taylor Ritenour <taylorritenour@berkeley.edu>
2012-07-05 12:24:39 -0700

    added lots of comments

--- src/genetic.cpp
+++ src/genetic.cpp
@@ -1,6 +1,25 @@
 /*
   -------------------------
   Genetic Algorithm
+
+  Attempts to imitate the process of evolution in a computer to solve problems. In this case, creating jazz.
+
+  Implementation Details:
+  Solutions are represented by a chromosome class defined in chromosome.cpp and chromosome.h. They are essentially
+  a wrapper for a vector of bytes with various assignment and altering methods.
+  The population of chromosomes that are put under the natural selection of this algorithm are represented as a
+  vector of chromosome objects.
+
+  Algorithm Details:
+  First, generate a population of random chromosomes.
+  Then enter the main loop by which we:
+  --Determine each one's fitness and declare a solution if it exists
+  --Randomly generate the number of members of the original population that will survive
+  --Pit two random members against one another and with a certain probability, 
+  the more fit of them will go on (eg. it is possible for the less fit one to win)
+  --Once the survivors have been determined (it is possible for a particular winner to have moved on more than once),
+  stochastically have them mate and mutate.
+  --Fill up the rest of the population with random new members
   -------------------------
 */
 
