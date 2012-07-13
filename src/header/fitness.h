82928cf78ed3ffca2bd3e69f5c8b11744d0a88d3
Donovan McKelvey <programmingmind@gmail.com>
2012-07-13 11:15:00 -0700

    fixed accompaniment stuff

--- src/header/fitness.h
+++ src/header/fitness.h
@@ -3,7 +3,15 @@
 
 #include "chromosome.h"
 
+#define ACCOMP_JAZZ 0
+#define ACCOMP_CLASS 1
+#define TOTAL_ACCOMP 2
+
 using namespace std;
+
+chromosome* accompaniments[TOTAL_ACCOMP];
+
+bool accompanimentSet = false;
 
 void createAccompaniment();
 
