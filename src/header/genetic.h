b5b94b58116d67d70bc3dfbb2b3f2e656ee86022
Taylor Ritenour <taylorritenour@berkeley.edu>
2012-07-25 10:00:34 -0700

    very fast. I generated a 950 soln in about 20 seconds

--- src/header/genetic.h
+++ src/header/genetic.h
@@ -8,12 +8,12 @@
 #define GENETIC_H
 
 #define CHROMO_LENGTH 385 //two sets of 12 bar blues
-#define CROSSOVER_RATE 0.95
+#define CROSSOVER_RATE 0.9
 #define WINNER_RATE 0.95
-#define MUTATION_RATE 0.3
+#define MUTATION_RATE 0.9 //I know that's high as balls, but man, it's working. Hilarious how I originally had it at 0.001
 #define POP_SIZE 100
-#define POP_REMAIN_LOWBOUND 80
+#define POP_REMAIN_LOWBOUND 98
 #define MAX_ITERATIONS 1000000
-#define FITNESS_THRESHOLD 900.0f
+#define FITNESS_THRESHOLD 950.0f
 
 #endif
